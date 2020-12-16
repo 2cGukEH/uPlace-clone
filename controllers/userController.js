7e858d24d09b3eb406b37d36f3318f8d1aaa08ed
김명재 <gimmyeongjae@gimmyeongjaeui-MacBookPro.local>
2020-12-16 16:19:38 +0900

    Initial Commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -100,8 +100,20 @@ export const logout = (req, res) => {
     res.redirect(routes.home);
 };
 
-export const getMe = (req, res) => {
-    res.render("userDetail", { pageTitle: "User Detail", user: req.user });
+export const getMe = async(req, res) => {
+    const {
+        params: { id },
+    } = req;
+    try {
+        const user = await User.findById(req.user.id).populate("videos");
+        console.log(req, user);
+        res.render("userDetail", {
+            pageTitle: "User Detail",
+            user: user,
+        });
+    } catch (error) {
+        res.redirect(routes.home);
+    }
 };
 
 export const userDetail = async(req, res) => {
@@ -109,7 +121,8 @@ export const userDetail = async(req, res) => {
         params: { id }
     } = req;
     try {
-        const user = await User.findById(id);
+        const user = await User.findById(id).populate("videos");
+        console.log(user);
         res.render("userDetail", { pageTitle: "User Detail", user });
     } catch (error) {
         res.redirect(routes.home);
