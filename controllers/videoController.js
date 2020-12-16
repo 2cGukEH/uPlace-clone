beaad2a498f050417bc018809a346fceb93fb3e4
김명재 <gimmyeongjae@gimmyeongjaeui-MacBookPro.local>
2020-12-16 22:43:25 +0900

    Delete error

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -73,11 +73,11 @@ export const getEditVideo = async(req, res) => {
     } = req;
     try {
         const video = await Video.findById(id);
-        if (video.creator.id !== req.user.id) {
+        console.log("!!!!", video.creator, req.user.id);
+        if (video.creator.toString() !== req.user.id) {
             throw Error();
         } else {
             res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
-            console.log(req.user);
         }
     } catch (error) {
         res.redirect(routes.home);
@@ -105,7 +105,7 @@ export const deleteVideo = async(req, res) => {
     } = req;
     try {
         const video = await Video.findById(id);
-        if (video.creator !== req.user.id) {
+        if (video.creator.id !== req.user.id) {
             throw Error();
         } else {
             await Video.findOneAndRemove({ _id: id });
