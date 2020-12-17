607120f96b6e45034622c90c0139dbf92019f67d
김명재 <gimmyeongjae@gimmyeongjaeui-MacBookPro.local>
2020-12-18 02:14:36 +0900

    Prestart_3

--- src/passport.js
+++ src/passport.js
@@ -15,18 +15,19 @@ passport.use(
             clientID: process.env.GH_ID,
             clientSecret: process.env.GH_SECRET,
             callbackURL: process.env.PRODUCTION ?
-                `https://polar-sea-27980.herokuapp.com${routes.githubCallback}` : `http://localhost:4000${routes.githubCallback}`
+                `https://pacific-scrubland-34681.herokuapp.com${routes.githubCallback}` : `http://localhost:4000${routes.githubCallback}`
         },
         githubLoginCallback
     )
 );
+
 
 passport.use(
     new GoogleStrategy({
             clientID: process.env.GG_ID,
             clientSecret: process.env.GG_SECRET,
             callbackURL: process.env.PRODUCTION ?
-                `https://enigmatic-spire-78020.herokuapp.com${routes.googleCallback}` : `http://localhost:4000${routes.googleCallback}`,
+                `hhttps://pacific-scrubland-34681.herokuapp.com${routes.googleCallback}` : `http://localhost:4000${routes.googleCallback}`,
         },
         googleLoginCallback
     )
